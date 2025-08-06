9e54d507230262e377e113de098dd1510db3c5a9
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-08-06 12:01:43 +0200

    feat(auth): issue access and refresh tokens on user registration and set refresh token as HTTP-only cookie

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import User from '../models/User.js';
 import { CustomError } from '../middleware/errorHandler.js';
+import { generateToken } from '../utils/generateToken.js';
 
 const router = express.Router();
 
@@ -18,7 +19,25 @@ router.post('/register', async (req, res) => {
 
   const user = await User.create({ name, email, password });
 
+  //Create Tokens
+  const payload = { userId: user._id.toString() };
+  const accessToken = await generateToken(payload, '1m');
+  const refreshToken = await generateToken(payload, '30d');
+
+  //Set refresh token in HTTP-Only cookie
+  res.cookie('refreshToken', refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    //If frontend and backend are stored on the same domain,
+    // then "sameSite" can be set as "lax" to make the app a bit more secure.
+    //To make frontend and backend stored on the same domain,
+    // a proxy can be created on vercel.
+    sameSite: 'none',
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+  });
+
   res.status(201).json({
+    accessToken,
     user: {
       id: user._id,
       name: user.name,
